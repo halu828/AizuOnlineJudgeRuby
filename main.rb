48ed164d5a8e7458421bdffcfda8befcb97f53f7
halu828 <halu7colors@gmail.com>
2014-07-22 03:56:02 +0900

    0002: Digit Number

--- main.rb
+++ main.rb
@@ -1,8 +1,5 @@
-height = []
-for i in 0..9
-    height[i] = gets.to_i
-end
-height.sort! {|a, b| b <=> a}
-for i in 0..2
-    puts height[i]
+while line = gets
+    a,b = line.split(" ")
+    sum = a.to_i + b.to_i
+    puts sum.to_s.length
 end
